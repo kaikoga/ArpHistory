8bd3e0dedbf64d8abadb9adb5fe9816890adefd4
kaikoga <info@mail.kaikoga.net>
2015-12-12 02:34:53 +0900

    primitive String fields may be null

--- src/net/kaikoga/arp/macro/valueTypes/MacroArpObjectPrimString.hx
+++ src/net/kaikoga/arp/macro/valueTypes/MacroArpObjectPrimString.hx
@@ -21,11 +21,15 @@ class MacroArpObjectPrimString implements IMacroArpObjectValueType {
 	}
 
 	public function readSelf(pos:Position, iFieldName:String):Expr {
-		return macro @:pos(pos) { this.$iFieldName = input.readUtf($v{iFieldName}); };
+		// if (nonNull) 
+		// return macro @:pos(pos) { this.$iFieldName = input.readUtf($v{iFieldName}); };
+		return macro @:pos(pos) { this.$iFieldName = net.kaikoga.arp.persistable.PersistableTool.readNullableUtf(input, $v{iFieldName}); };
 	}
 
 	public function writeSelf(pos:Position, iFieldName:String):Expr {
-		return macro @:pos(pos) { output.writeUtf($v{iFieldName}, this.$iFieldName); };
+		// if (nonNull) 
+		// return macro @:pos(pos) { output.writeUtf($v{iFieldName}, this.$iFieldName); };
+		return macro @:pos(pos) { net.kaikoga.arp.persistable.PersistableTool.writeNullableUtf(output, $v{iFieldName}, this.$iFieldName); };
 	}
 }
 
