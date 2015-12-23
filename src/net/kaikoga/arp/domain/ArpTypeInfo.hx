3a8e625a9cbcd3d3883e8262326338f7570f5430
kaikoga <info@mail.kaikoga.net>
2015-12-24 04:35:12 +0900

    defaults to default template if arpType string equals template name

--- src/net/kaikoga/arp/domain/ArpTypeInfo.hx
+++ src/net/kaikoga/arp/domain/ArpTypeInfo.hx
@@ -7,6 +7,8 @@ class ArpTypeInfo {
 	public var name(default, null):String;
 	public var arpType(default, null):ArpType;
 
+	public function isDefault():Bool return name == arpType.toString();
+
 	public function new(name:String, arpType:ArpType) {
 		this.name = name;
 		this.arpType = arpType;
--- src/net/kaikoga/arp/domain/gen/ArpObjectGenerator.hx
+++ src/net/kaikoga/arp/domain/gen/ArpObjectGenerator.hx
@@ -4,9 +4,9 @@ import net.kaikoga.arp.domain.core.ArpType;
 
 class ArpObjectGenerator<T:IArpObject> extends ArpDynamicGenerator<T> {
 
-	public function new(dest:Class<T>, isDefault:Bool = false) {
+	public function new(dest:Class<T>, forceDefault:Null<Bool> = null) {
 		var arpTypeInfo:ArpTypeInfo = Type.createEmptyInstance(dest).arpTypeInfo();
-		super(arpTypeInfo.arpType, dest, arpTypeInfo.name, isDefault);
+		super(arpTypeInfo.arpType, dest, arpTypeInfo.name, (forceDefault != null) ? forceDefault : arpTypeInfo.isDefault());
 	}
 
 }
