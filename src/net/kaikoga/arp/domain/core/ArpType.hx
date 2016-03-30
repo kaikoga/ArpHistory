2315e3b434bfcd44988cca50da645587915b95dc
kaikoga <info@mail.kaikoga.net>
2016-03-31 03:29:54 +0900

    some helper @:from ArpType

--- src/net/kaikoga/arp/domain/core/ArpType.hx
+++ src/net/kaikoga/arp/domain/core/ArpType.hx
@@ -4,6 +4,12 @@ abstract ArpType(String) {
 	inline public function new(value:String) {
 		this = value;
 	}
+	@:from inline public static function fromArpTypeInfo(arpTypeInfo:ArpTypeInfo):ArpType {
+		return arpTypeInfo.arpType;
+	}
+	@:from inline public static function fromClass<T:IArpObject>(klass:Class<T>):ArpType {
+		return untyped klass._arpTypeInfo.arpType;
+	}
 	@:to inline public function toString():String {
 		return this;
 	}
