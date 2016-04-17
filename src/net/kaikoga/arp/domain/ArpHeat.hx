52bfb584db3b675c5a02282f1d9d42bc4b732437
kaikoga <info@mail.kaikoga.net>
2016-04-18 06:58:47 +0900

    ArpHeat is comparable

--- src/net/kaikoga/arp/domain/ArpHeat.hx
+++ src/net/kaikoga/arp/domain/ArpHeat.hx
@@ -4,4 +4,11 @@ package net.kaikoga.arp.domain;
 	var Cold = 1;
 	var Warming = 2;
 	var Warm = 3;
+
+	@:op(a > b) static function opGt( a:ArpHeat, b:ArpHeat):Bool;
+	@:op(a < b) static function opLt( a:ArpHeat, b:ArpHeat):Bool;
+	@:op(a >= b) static function opGte( a:ArpHeat, b:ArpHeat):Bool;
+	@:op(a <= b) static function opLte( a:ArpHeat, b:ArpHeat):Bool;
+	@:op(a == b) static function opEq( a:ArpHeat, b:ArpHeat):Bool;
+	@:op(a != b) static function opNeq( a:ArpHeat, b:ArpHeat):Bool;
 }
