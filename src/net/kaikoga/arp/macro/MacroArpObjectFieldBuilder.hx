590ef4ff68edf37a472386da1afc00a9c7d56ead
kaikoga <info@mail.kaikoga.net>
2016-05-04 15:10:48 +0900

    implement following types on guessing NativeFieldType

--- src/net/kaikoga/arp/macro/MacroArpObjectFieldBuilder.hx
+++ src/net/kaikoga/arp/macro/MacroArpObjectFieldBuilder.hx
@@ -2,7 +2,6 @@ package net.kaikoga.arp.macro;
 
 #if macro
 
-import haxe.macro.Printer;
 import net.kaikoga.arp.macro.fields.ds.MacroArpObjectReferenceSetField;
 import net.kaikoga.arp.macro.fields.ds.MacroArpObjectReferenceListField;
 import net.kaikoga.arp.macro.fields.ds.MacroArpObjectReferenceOmapField;
@@ -25,6 +24,8 @@ import net.kaikoga.arp.macro.fields.MacroArpObjectReferenceField;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.Type;
+import haxe.macro.TypeTools;
+import haxe.macro.Printer;
 
 class MacroArpObjectFieldBuilder {
 
@@ -37,20 +38,25 @@ class MacroArpObjectFieldBuilder {
 		this.pos = definition.nativeField.pos;
 	}
 
-	private function typeParam(typePath:TypePath, index:Int = 0):ComplexType {
+	private function typeParam(type:Type, index:Int = 0):Type {
 		try {
-			if (typePath.params == null) throw "invalid type parameter";
-			if (typePath.params.length <= index) throw "invalid type parameter";
-			switch (typePath.params[index]) {
-				case TypeParam.TPType(t): return t;
-				case TypeParam.TPExpr(e): throw "invalid type parameter";
+			switch (type) {
+				case
+					Type.TEnum(_, params),
+					Type.TInst(_, params),
+					Type.TType(_, params),
+					Type.TAbstract(_, params):
+					if (params.length <= index) throw "invalid type";
+					return params[index];
+				case _: throw "invalid type";
 			}
 		} catch (e:String) {
-			throw "invalid type parameter " + new Printer().printTypePath(typePath);
+			throw "invalid type" + new Printer().printComplexType(TypeTools.toComplexType(type));
 		}
 	}
 
-	private function fqnToNativeFieldType(fqn:Array<String>, typePath:TypePath):Null<MacroArpObjectNativeFieldType> {
+	private function fqnToNativeFieldType(fqn:Array<String>, type:Type):Null<MacroArpObjectNativeFieldType> {
+		this.log("fqn=" + fqn.join("."));
 		switch (fqn.join(".")) {
 			case "Int":
 				return MacroArpObjectNativeFieldType.NativeValueType(new MacroArpObjectPrimInt());
@@ -61,19 +67,19 @@ class MacroArpObjectFieldBuilder {
 			case "String":
 				return MacroArpObjectNativeFieldType.NativeValueType(new MacroArpObjectPrimString());
 			case "Array":
-				return MacroArpObjectNativeFieldType.NativeStdArray(complexTypeToNativeFieldType(typeParam(typePath)));
+				return MacroArpObjectNativeFieldType.NativeStdArray(typeToNativeFieldType(typeParam(type)));
 			case "List":
-				return MacroArpObjectNativeFieldType.NativeStdList(complexTypeToNativeFieldType(typeParam(typePath)));
-			case "Map", "Map.IMap", "haxe.Constraints.IMap":
-				return MacroArpObjectNativeFieldType.NativeStdMap(complexTypeToNativeFieldType(typeParam(typePath, 1)));
-			case "net.kaikoga.arp.ds.ISet", "net.kaikoga.arp.ds.impl.ArraySet", "net.kaikoga.arp.domain.ds.ArpObjectSet":
-				return MacroArpObjectNativeFieldType.NativeDsISet(complexTypeToNativeFieldType(typeParam(typePath)));
-			case "net.kaikoga.arp.ds.IList", "net.kaikoga.arp.ds.impl.ArrayList", "net.kaikoga.arp.domain.ds.ArpObjectList":
-				return MacroArpObjectNativeFieldType.NativeDsIList(complexTypeToNativeFieldType(typeParam(typePath)));
-			case "net.kaikoga.arp.ds.IMap", "net.kaikoga.arp.ds.impl.StdMap", "net.kaikoga.arp.domain.ds.ArpObjectMap":
-				return MacroArpObjectNativeFieldType.NativeDsIMap(complexTypeToNativeFieldType(typeParam(typePath, 1)));
-			case "net.kaikoga.arp.ds.IOmap", "net.kaikoga.arp.ds.impl.StdOmap", "net.kaikoga.arp.domain.ds.ArpObjectOmap":
-				return MacroArpObjectNativeFieldType.NativeDsIOmap(complexTypeToNativeFieldType(typeParam(typePath, 1)));
+				return MacroArpObjectNativeFieldType.NativeStdList(typeToNativeFieldType(typeParam(type)));
+			case "haxe.IMap", "haxe.Constraints.IMap":
+				return MacroArpObjectNativeFieldType.NativeStdMap(typeToNativeFieldType(typeParam(type, 1)));
+			case "net.kaikoga.arp.ds.ISet":
+				return MacroArpObjectNativeFieldType.NativeDsISet(typeToNativeFieldType(typeParam(type)));
+			case "net.kaikoga.arp.ds.IList":
+				return MacroArpObjectNativeFieldType.NativeDsIList(typeToNativeFieldType(typeParam(type)));
+			case "net.kaikoga.arp.ds.IMap":
+				return MacroArpObjectNativeFieldType.NativeDsIMap(typeToNativeFieldType(typeParam(type, 1)));
+			case "net.kaikoga.arp.ds.IOmap":
+				return MacroArpObjectNativeFieldType.NativeDsIOmap(typeToNativeFieldType(typeParam(type, 1)));
 			case "net.kaikoga.arp.structs.ArpArea2d":
 				return MacroArpObjectNativeFieldType.NativeValueType(new MacroArpObjectArpStruct(macro :net.kaikoga.arp.structs.ArpArea2d));
 			case "net.kaikoga.arp.structs.ArpColor":
@@ -88,59 +94,79 @@ class MacroArpObjectFieldBuilder {
 				return MacroArpObjectNativeFieldType.NativeValueType(new MacroArpObjectArpStruct(macro :net.kaikoga.arp.structs.ArpPosition));
 			case "net.kaikoga.arp.structs.ArpRange":
 				return MacroArpObjectNativeFieldType.NativeValueType(new MacroArpObjectArpStruct(macro :net.kaikoga.arp.structs.ArpRange));
+			case "net.kaikoga.arp.domain.IArpObject":
+				return MacroArpObjectNativeFieldType.NativeMaybeReference(TypeTools.toComplexType(type));
 			default:
 				return null;
 		}
 	}
 
-	private function complexTypeToNativeFieldType(complexType:ComplexType):MacroArpObjectNativeFieldType {
-		var typePath:TypePath;
-		switch (complexType) {
-			case ComplexType.TPath(p):
-				typePath = p;
-			default:
-				return MacroArpObjectNativeFieldType.NativeInvalid;
-		}
-
-		var type:haxe.macro.Type;
-		var fqn:Array<String>;
-		try {
-			type = Context.resolveType(complexType, this.pos);
-			type = Context.follow(type);
-		}
-
+	private function baseTypeToNativeFieldType(type:Type, baseType:Type):MacroArpObjectNativeFieldType {
 		var result:MacroArpObjectNativeFieldType = null;
-		switch (type) {
-			case haxe.macro.Type.TInst(classRef, params):
+		var fqn:Array<String>;
+		switch (baseType) {
+			case Type.TInst(classRef, params):
 				var classType:ClassType = classRef.get();
 				fqn = classType.pack.copy();
 				fqn.push(classType.name);
-				result = fqnToNativeFieldType(fqn, typePath);
+				result = fqnToNativeFieldType(fqn, type);
 				if (result != null) return result;
-				// TODO follow super class and interface
-				return MacroArpObjectNativeFieldType.NativeMaybeReference(complexType);
-			case haxe.macro.Type.TAbstract(abstractRef, params):
+				// follow super class and interfaces
+				if (classType.superClass != null) {
+					var superClassType:Type = Type.TInst(classType.superClass.t, classType.superClass.params);
+					this.log("extends=" + Std.string(superClassType));
+					result = baseTypeToNativeFieldType(type, superClassType);
+					if (result != null) return result;
+				}
+				for (intf in classType.interfaces) {
+					var interfaceType:Type = Type.TInst(intf.t, intf.params);
+					this.log("implements=" + Std.string(interfaceType));
+					result = baseTypeToNativeFieldType(type, interfaceType);
+					if (result != null) return result;
+				}
+			case Type.TAbstract(abstractRef, params):
 				var abstractType:AbstractType = abstractRef.get();
 				fqn = abstractType.pack.copy();
 				fqn.push(abstractType.name);
-				result = fqnToNativeFieldType(fqn, typePath);
+				result = fqnToNativeFieldType(fqn, type);
 				if (result != null) return result;
-				// TODO follow abstract
-				return MacroArpObjectNativeFieldType.NativeMaybeReference(complexType);
-			case haxe.macro.Type.TType(typeRef, params):
-				// TODO follow typedef
-				return MacroArpObjectNativeFieldType.NativeMaybeReference(complexType);
-			default:
+				// follow underlying type
+				this.log("underlying=" + Std.string(abstractType.type));
+				result = baseTypeToNativeFieldType(type, abstractType.type);
+				if (result != null) return result;
+			case Type.TType(defTypeRef, params):
+				var defType:DefType = defTypeRef.get();
+				// follow underlying type
+				this.log("resolve=" + Std.string(defType.type));
+				result = baseTypeToNativeFieldType(type, defType.type);
+				if (result != null) return result;
+			case _:
 		}
+		return null;
+	}
+
+	private function typeToNativeFieldType(type:Type):MacroArpObjectNativeFieldType {
+		this.log("Type=" + Std.string(type));
+		var result = baseTypeToNativeFieldType(type, type);
+		if (result != null) return result;
 		return MacroArpObjectNativeFieldType.NativeInvalid;
 	}
 
-	private function run():IMacroArpObjectField {
+	private function complexTypeToNativeFieldType(complexType:ComplexType):MacroArpObjectNativeFieldType {
+		// we have to follow types in typed universe
+		var type:Type = Context.resolveType(complexType, this.pos);
+		return typeToNativeFieldType(type);
+	}
+
+	private function doRun():IMacroArpObjectField {
 		if (!this.definition.isValidNativeType()) return null;
+		this.log("ComplexType=" + new Printer().printComplexType(this.definition.nativeType));
 
 		var complexType:ComplexType = this.definition.nativeType;
 		var pos:Position = this.definition.nativeField.pos;
 		var nativeFieldType:MacroArpObjectNativeFieldType = complexTypeToNativeFieldType(complexType);
+
+		this.log("NativeType=" + Std.string(nativeFieldType));
 
 		switch (nativeFieldType) {
 			case MacroArpObjectNativeFieldType.NativeValueType(p):
@@ -206,8 +232,21 @@ class MacroArpObjectFieldBuilder {
 		return null;
 	}
 
+	private function run():IMacroArpObjectField {
+		this.log("field: " + definition.nativeField.name);
+		var result = doRun();
+		this.log("done: " + definition.nativeField.name);
+		return result;
+	}
+
 	public static function fromDefinition(definition:MacroArpObjectFieldDefinition):IMacroArpObjectField {
 		return new MacroArpObjectFieldBuilder(definition).run();
+	}
+
+	inline private function log(message:String):Void {
+		#if arp_macro_verbose
+		Context.warning(message, this.definition.nativeField.pos);
+		#end
 	}
 }
 
