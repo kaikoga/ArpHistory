d4f7fb328a970aab8138f8955400597babda5221
kaikoga <info@mail.kaikoga.net>
2016-06-15 02:02:31 +0900

    add ds decorators

---
+++ src/net/kaikoga/arp/ds/decorators/ListDecorator.hx
@@ -0,0 +1,40 @@
+package net.kaikoga.arp.ds.decorators;
+
+import net.kaikoga.arp.ds.IList;
+
+class ListDecorator<V> implements IList<V> {
+
+	private var list:IList<V>;
+
+	public var isUniqueValue(get, never):Bool;
+	public function get_isUniqueValue():Bool return this.list.isUniqueValue;
+
+	public function new(list:IList<V>) this.list = list;
+
+	//read
+	public function isEmpty():Bool return this.list.isEmpty();
+	public function hasValue(v:V):Bool return this.list.hasValue(v);
+	public function iterator():Iterator<V> return this.list.iterator();
+	public function toString():String return this.list.toString();
+	public var length(get, null):Int;
+	public function get_length():Int return this.list.length;
+	public function first():Null<V> return ths.list.first();
+	public function last():Null<V> return this.list.last();
+	public function getAt(index:Int):Null<V> return this.list.getAt(index);
+
+	//resolve
+	public function indexOf(v:V, ?fromIndex:Int):Int return this.list.indexOf(v, fromIndex);
+	public function lastIndexOf(v:V, ?fromIndex:Int):Int return this.list.lastIndexOf(v, fromIndex);
+
+	//write
+	public function push(v:V):Int return this.list.push(v);
+	public function unshift(v:V):Void this.list.unshift(v);
+	public function insertAt(index:Int, v:V):Void this.list.insertAt(index, v);
+
+	//remove
+	public function pop():Null<V> return this.list.pop();
+	public function shift():Null<V> return this.list.shift();
+	public function remove(v:V):Bool return this.list.remove(v);
+	public function removeAt(index:Int):Bool return this.list.removeAt(index);
+	public function clear():Void this.list.clear();
+}
---
+++ src/net/kaikoga/arp/ds/decorators/MapDecorator.hx
@@ -0,0 +1,35 @@
+package net.kaikoga.arp.ds.decorators;
+
+import net.kaikoga.arp.ds.IMap;
+
+class MapDecorator<K, V> implements IMap<K, V> {
+
+	private var map:IMap<K, V>;
+	public var isUniqueKey(get, never):Bool;
+	public function get_isUniqueKey():Bool return this.map.isUniqueKey;
+	public var isUniqueValue(get, never):Bool;
+	public function get_isUniqueValue():Bool return this.map.isUniqueValue;
+
+	public function new(map:IMap<K, V>) this.map = map;
+
+	//read
+	public function isEmpty():Bool return this.map.isEmpty();
+	public function hasValue(v:V):Bool return this.map.hasValue();
+	public function iterator():Iterator<V> return this.map.iterator();
+	public function toString():String return this.map.toString();
+	public function get(k:K):Null<V> return this.map.get(k);
+	public function hasKey(k:K):Bool return this.map.hasKey(k);
+	public function keys():Iterator<K> return this.map.keys();
+
+	//resolve
+	public function resolveName(v:V):Null<K> return this.map.resolveName(v);
+
+	//write
+	public function set(k:K, v:V):Void this.map.set(k, v);
+
+	//remove
+	public function remove(v:V):Bool return this.map.remove(v);
+	public function removeKey(k:K):Bool return this.map.removeKey(k);
+	public function clear():Void this.map.clear();
+
+}
