bbcfc950525c7486d404e50215a6ac653e9dfced
kaikoga <info@mail.kaikoga.net>
2017-01-05 19:53:34 +0900

    move TiledExternal from ArpEngine

--- tests/net/kaikoga/arpx/ArpThirdpartyComponentsCase.hx
+++ tests/net/kaikoga/arpx/ArpThirdpartyComponentsCase.hx
@@ -1,5 +1,6 @@
 ﻿package net.kaikoga.arpx;
 
+import net.kaikoga.arpx.external.TiledExternal;
 import net.kaikoga.arpx.tileMap.FmfFileTileMap;
 
 import picotest.PicoAssert.*;
@@ -7,6 +8,8 @@ import picotest.PicoAssert.*;
 class ArpThirdpartyComponentsCase {
 
 	public function testFields() {
+		assertNotNull(new TiledExternal());
+
 		assertNotNull(new FmfFileTileMap());
 	}
 
