1a8394609744c69539541d69b5f977cbe2b07004
kaikoga <info@mail.kaikoga.net>
2017-01-18 01:27:48 +0900

    cache symbols

--- src/net/kaikoga/arp/utils/ArpIdGenerator.hx
+++ src/net/kaikoga/arp/utils/ArpIdGenerator.hx
@@ -1,13 +1,21 @@
 package net.kaikoga.arp.utils;
 
 abstract ArpIdGenerator(Int) {
+
 	inline private static var AUTO_HEADER:String = "$";
+	private static var symbols:Array<String> = [];
+
 	inline public function new() {
 		this = 0;
 	}
+
 	inline public function next():String {
-		return '$AUTO_HEADER${Std.string(this++)}';
+		var result = symbols[this];
+		if (result == null) symbols[this] = result = '$AUTO_HEADER${Std.string(this)}';
+		this++;
+		return result;
 	}
+
 	inline public function reset():Void {
 		this = 0;
 	}
