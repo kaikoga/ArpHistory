7aaa658e06d31a677c0d23b095981863973de813
kaikoga <info@mail.kaikoga.net>
2017-05-04 05:18:53 +0900

    oops

--- src/net/kaikoga/arp/ds/access/IOmapResolve.hx
+++ src/net/kaikoga/arp/ds/access/IOmapResolve.hx
@@ -1,5 +1,5 @@
 package net.kaikoga.arp.ds.access;
 
-interface IOmapResolve<K, V> extends IOmapWrite<K, V> extends IMapResolve<K, V>  extends IListResolve<V> {
+interface IOmapResolve<K, V> extends IOmapRead<K, V> extends IMapResolve<K, V>  extends IListResolve<V> {
 	function resolveKeyIndex(k:K):Int;
 }
