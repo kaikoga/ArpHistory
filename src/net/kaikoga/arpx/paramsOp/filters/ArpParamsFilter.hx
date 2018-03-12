62fc19491c576580305e434fba127326e85b5f3f
kaikoga <info@mail.kaikoga.net>
2018-03-12 21:22:11 +0900

    follow Haxe 4.0.0-preview3

--- src/net/kaikoga/arpx/paramsOp/filters/ArpParamsFilter.hx
+++ src/net/kaikoga/arpx/paramsOp/filters/ArpParamsFilter.hx
@@ -9,11 +9,11 @@ class ArpParamsFilter implements IArpParamsRead {
 	private var params:IArpParamsRead;
 	public function new(params:IArpParamsRead) this.params = params;
 
-	public function getInt(key:String, defaultValue:Int = null):Null<Int> return ArpParamsMacros.getSafe(key, defaultValue);
-	public function getFloat(key:String, defaultValue:Float = null):Null<Float> return ArpParamsMacros.getSafe(key, defaultValue);
-	public function getString(key:String, defaultValue:String = null):String return ArpParamsMacros.getSafe(key, defaultValue);
-	public function getBool(key:String, defaultValue:Bool = null):Null<Bool> return ArpParamsMacros.getSafe(key, defaultValue);
-	public function getArpDirection(key:String, defaultValue:ArpDirection = null):ArpDirection return ArpParamsMacros.getSafe(key, defaultValue);
+	public function getInt(key:String, defaultValue:Int = null):Null<Int> return ArpParamsMacros.getSafe(key, defaultValue, Int);
+	public function getFloat(key:String, defaultValue:Float = null):Null<Float> return ArpParamsMacros.getSafe(key, defaultValue, Float);
+	public function getString(key:String, defaultValue:String = null):String return ArpParamsMacros.getSafe(key, defaultValue, String);
+	public function getBool(key:String, defaultValue:Bool = null):Null<Bool> return ArpParamsMacros.getSafe(key, defaultValue, Bool);
+	public function getArpDirection(key:String, defaultValue:ArpDirection = null):ArpDirection return ArpParamsMacros.getSafe(key, defaultValue, ArpDirection);
 
 	public function getAsString(key:String, defaultValue:String = null):String return ArpParamsMacros.getAsString(key, defaultValue);
 
