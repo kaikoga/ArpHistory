0bdeb158a938928918fa37940f6d2eeece310471
kaikoga <info@mail.kaikoga.net>
2018-03-22 02:21:30 +0900

    follow new function type syntax

--- src/net/kaikoga/arp/task/Tickable.hx
+++ src/net/kaikoga/arp/task/Tickable.hx
@@ -1,7 +1,7 @@
 package net.kaikoga.arp.task;
 
 class Tickable implements ITickable {
-	public function new(tick:Float->Bool) this._tick = tick;
+	public function new(tick:(timeslice:Float)->Bool) this._tick = tick;
 	private dynamic function _tick(timeslice:Float):Bool return false;
 	inline public function tick(timeslice:Float):Bool return this._tick(timeslice);
 }
