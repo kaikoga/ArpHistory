fe6dd913e1b0323b596b3a69622b1013a8491681
kaikoga <info@mail.kaikoga.net>
2018-03-22 02:03:26 +0900

    follow short lambdas

--- src/net/kaikoga/arpx/backends/flash/field/FieldFlashImpl.hx
+++ src/net/kaikoga/arpx/backends/flash/field/FieldFlashImpl.hx
@@ -23,7 +23,7 @@ class FieldFlashImpl extends ArpObjectImplBase implements IFieldFlashImpl {
 
 	inline public static function copySortedMortals(mortals:Iterable<Mortal>, bitmapData:BitmapData, transform:ITransform):Void {
 		var temp:Array<Mortal> = [for (m in mortals) m];
-		ArraySort.sort(temp, function(a:Mortal, b:Mortal) return Reflect.compare(a.position.y + a.position.z, b.position.y + b.position.z));
+		ArraySort.sort(temp, (a:Mortal, b:Mortal) -> Reflect.compare(a.position.y + a.position.z, b.position.y + b.position.z));
 		for (m in temp) {
 			m.copySelf(bitmapData, transform);
 		}
