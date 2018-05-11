f31b8839b5c2913ca8e61928e1a899aed2df2d5d
kaikoga <info@mail.kaikoga.net>
2018-05-11 21:18:25 +0900

    use GPU rendering for tiles

--- src/net/kaikoga/arpx/impl/backends/heaps/chip/TextureChipHeapsImpl.hx
+++ src/net/kaikoga/arpx/impl/backends/heaps/chip/TextureChipHeapsImpl.hx
@@ -40,31 +40,24 @@ class TextureChipHeapsImpl extends ArpObjectImplBase implements IChipImpl {
 			return;
 		}
 
-		var pt:Point = transform.asPoint();
-		if (pt != null) {
-			var bitmap:Bitmap = new Bitmap(tile, context.buf);
-			bitmap.x = pt.x;
-			bitmap.y = pt.y;
-		} else {
-			/*
-			var colorTransform:ColorTransform = null;
-			if (params.getBool("tint")) {
-				var ra:Null<Float> = params.getFloat("redMultiplier", 1.0);
-				var ga:Null<Float> = params.getFloat("greenMultiplier", 1.0);
-				var ba:Null<Float> = params.getFloat("blueMultiplier", 1.0);
-				var aa:Null<Float> = params.getFloat("alphaMultiplier", 1.0);
-				var rb:Null<Float> = params.getFloat("redOffset", 0.0);
-				var gb:Null<Float> = params.getFloat("greenOffset", 0.0);
-				var bb:Null<Float> = params.getFloat("blueOffset", 0.0);
-				var ab:Null<Float> = params.getFloat("alphaOffset", 0.0);
-				colorTransform = new ColorTransform(ra, ga, ba, aa, rb, gb, bb, ab);
-			}
-			var blendMode:BlendMode = cast params.getAsString("blendMode");
-			g2.pushTransformation(FastMatrix3.fromMatrix3(transform.asMatrix()));
-			g2.drawSubImage(this.chip.texture.image(), 0, 0, faceInfo.sx, faceInfo.sy, faceInfo.sw, faceInfo.sh);
-			g2.popTransformation();
-			*/
+		context.pushTransform(transform);
+		context.drawTile(tile);
+		context.popTransform();
+		/*
+		var colorTransform:ColorTransform = null;
+		if (params.getBool("tint")) {
+			var ra:Null<Float> = params.getFloat("redMultiplier", 1.0);
+			var ga:Null<Float> = params.getFloat("greenMultiplier", 1.0);
+			var ba:Null<Float> = params.getFloat("blueMultiplier", 1.0);
+			var aa:Null<Float> = params.getFloat("alphaMultiplier", 1.0);
+			var rb:Null<Float> = params.getFloat("redOffset", 0.0);
+			var gb:Null<Float> = params.getFloat("greenOffset", 0.0);
+			var bb:Null<Float> = params.getFloat("blueOffset", 0.0);
+			var ab:Null<Float> = params.getFloat("alphaOffset", 0.0);
+			colorTransform = new ColorTransform(ra, ga, ba, aa, rb, gb, bb, ab);
 		}
+		var blendMode:BlendMode = cast params.getAsString("blendMode");
+		*/
 	}
 }
 
--- src/net/kaikoga/arpx/impl/backends/heaps/display/DisplayContext.hx
+++ src/net/kaikoga/arpx/impl/backends/heaps/display/DisplayContext.hx
@@ -2,12 +2,12 @@ package net.kaikoga.arpx.impl.backends.heaps.display;
 
 #if arp_backend_heaps
 
-import net.kaikoga.arpx.geom.APoint;
-import h2d.Bitmap;
 import h2d.Tile;
 import h2d.Sprite;
+import h3d.Engine;
 import net.kaikoga.arpx.display.DisplayContextBase;
 import net.kaikoga.arpx.display.IDisplayContext;
+import net.kaikoga.arpx.geom.AMatrix;
 import net.kaikoga.arpx.geom.ITransform;
 
 class DisplayContext extends DisplayContextBase implements IDisplayContext {
@@ -20,26 +20,31 @@ class DisplayContext extends DisplayContextBase implements IDisplayContext {
 	public var height(get, null):Int;
 	private function get_height():Int return _height;
 
+	private var renderContext:RenderContext;
+	
 	public function new(buf:Sprite, width:Int, height:Int, transform:ITransform = null, clearColor:UInt = 0) {
 		super(transform, clearColor);
 		this.buf = buf;
 		this._width = width;
 		this._height = height;
+		this.renderContext = new RenderContext(Engine.getCurrent());
 	}
 
-	public function start():Void this.buf.removeChildren();
-	public function display():Void return;
+	public function start():Void {
+		this.buf.removeChildren();
+		this.renderContext.start();
+	}
+	public function display():Void this.renderContext.display();
 
 	public function fillRect(l:Int, t:Int, w:Int, h:Int, color:UInt):Void {
-		var pt:APoint = transform.asPoint();
-		if (pt != null) {
-			var tile:Tile = Tile.fromColor(color);
-			var bitmap:Bitmap = new Bitmap(tile, buf);
-			bitmap.x = pt.x + l;
-			bitmap.y = pt.y + t;
-			bitmap.scaleX = w;
-			bitmap.scaleY = h;
-		}
+		var matrix:AMatrix = transform.concatTransform(new AMatrix(w, 0, 0, h, l, t)).asMatrix();
+		var tile:Tile = Tile.fromColor(color);
+		this.renderContext.renderTile(matrix, tile);
+	}
+
+	public function drawTile(tile:Tile):Void {
+		var matrix:AMatrix = transform.concatTransform(new AMatrix(tile.width, 0, 0, tile.height, 0, 0)).asMatrix();
+		this.renderContext.renderTile(matrix, tile);
 	}
 
 }
