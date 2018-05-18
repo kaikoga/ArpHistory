0554e0946d2c7751b805e5cc8a1c3b86fcfb5789
kaikoga <info@mail.kaikoga.net>
2018-05-19 04:34:17 +0900

    add ResourceTextData

---
+++ src/net/kaikoga/arpx/text/ResourceTextData.hx
@@ -0,0 +1,20 @@
+package net.kaikoga.arpx.text;
+
+import haxe.Resource;
+import net.kaikoga.arp.structs.ArpParams;
+
+@:arpType("text", "resource")
+class ResourceTextData extends TextData {
+
+	@:arpField public var src:String;
+
+	public function new() {
+		super();
+	}
+
+	override public function publish(params:ArpParams = null):String {
+		return Resource.getString(this.src);
+	}
+}
+
+
