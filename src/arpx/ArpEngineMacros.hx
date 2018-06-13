d3d00e3f1975c2988d55c6efc34f9616967aa14a
kaikoga <info@mail.kaikoga.net>
2018-06-13 20:57:49 +0900

    guess backend based on defines, do not define arp_backend

--- src/arpx/ArpEngineMacros.hx
+++ src/arpx/ArpEngineMacros.hx
@@ -4,44 +4,68 @@ package arpx;
 
 import haxe.macro.Compiler;
 import haxe.macro.Context;
+import haxe.macro.Expr;
 
 class ArpEngineMacros {
 
-	public static function init():Void {
-		if (getFlag("arp_engine_init")) return;
-		setFlag("arp_engine_init");
+	private static var instance:ArpEngineMacros;
 
-		if (getFlag("arp_backend_flash")) setDefine("arp_backend", "flash");
-		if (getFlag("arp_backend_openfl")) setDefine("arp_backend", "openfl");
-		if (getFlag("arp_backend_heaps")) setDefine("arp_backend", "heaps");
-
-		var dArpBackend:String = getDefine("arp_backend");
-		if (dArpBackend == null) {
-			if (getFlag("flash") ) {
-				setFlag("arp_backend_flash");
-			} else {
-				setFlag("arp_backend_openfl");
-			}
-		}
+	public static function init():Array<Field> {
+		if (instance == null) instance = new ArpEngineMacros();
+		return null;
 	}
 
-	inline private static function getDefine(name:String):String {
-		return Context.definedValue(name);
-	}
-	inline private static function getFlag(name:String):Bool {
-		return getDefine(name) != null;
-	}
+	private function new() { 
+		var components:Array<ArpEngineBackend> = [];
+		// components.push(new ArpEngineBackend("arp_texture_backend", ["heaps", "flash", "openfl"]));
 
-	inline private static function setDefine(name:String, value:String):String {
-		if (getDefine(name) != null) throw 'Failed to set ${name} to ${value}, was ${getDefine(name)}'
-		Compiler.define(name, value);
-		return value;
+		var root:ArpEngineBackend = new ArpEngineBackend("arp_backend", ["heaps", "flash", "openfl"]);
+		var rootBackend:String = root.guessBackend(null);
+		
+		for (component in components) component.guessBackend(rootBackend);
+		return;
 	}
-	inline private static function setFlag(name:String, value:Bool = true):Bool {
-		setDefine(name, value ? "1" : null);
-		return value;
-	}
-
 }
 
+class ArpEngineBackend {
+
+	private var category:String;
+	private var target:String;
+	private var targets:Array<String>;
+
+	private function fullName(target:String):String return '${this.category}_${target}';
+
+	public function new(category:String, targets:Array<String>) {
+		this.category = category;
+		this.targets = targets;
+		this.consumeDefines();
+	}
+
+	private function consumeDefines() {
+		var flagsDefined:Array<String> = [];
+		for (target in this.targets) {
+			var flagName:String = fullName(target);
+			if (Context.defined(flagName)) flagsDefined.push(flagName);
+		}
+		switch (flagsDefined.length) {
+			case 0:
+			case 1: this.target = flagsDefined[0];
+			case _: throw 'Compiler flag conflict: Backends cannot coexist: ${flagsDefined.join(",")}';
+		} 
+	}
+	
+	public function guessBackend(rootBackend:String):String {
+		if (this.target != null) return this.target;
+
+		if (rootBackend != null) this.targets.unshift(rootBackend);
+		for (target in this.targets) {
+			this.target = target;
+			if (Context.defined(target)) break;
+		}
+		if (rootBackend != null) this.targets.shift();
+
+		Compiler.define(fullName(target));
+		return this.target;
+	} 
+}
 #end
