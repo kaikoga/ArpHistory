610ebed9f3ea1494d5b420955020493aeeed3d01
kaikoga <info@mail.kaikoga.net>
2018-08-11 02:49:26 +0900

    refactor: promote GridTextureImpl

--- src/arpx/impl/cross/texture/decorators/GridTextureImpl.hx
+++ src/arpx/impl/cross/texture/decorators/GridTextureImpl.hx
@@ -1,11 +1,49 @@
 package arpx.impl.cross.texture.decorators;
 
-#if (arp_display_backend_flash || arp_display_backend_openfl)
-typedef GridTextureImpl = arpx.impl.flash.texture.decorators.GridTextureImpl;
-#elseif arp_display_backend_heaps
-typedef GridTextureImpl = arpx.impl.heaps.texture.decorators.GridTextureImpl;
-#elseif arp_display_backend_sys
-typedef GridTextureImpl = arpx.impl.sys.texture.decorators.GridTextureImpl;
-#elseif arp_display_backend_stub
-typedef GridTextureImpl = arpx.impl.stub.texture.decorators.GridTextureImpl;
-#end
+import arpx.impl.cross.texture.decorators.MultiTextureImplBase;
+import arpx.texture.decorators.GridTexture;
+
+class GridTextureImpl extends MultiTextureImplBase<GridTexture> implements ITextureImpl {
+
+	public function new(texture:GridTexture) {
+		super(texture);
+	}
+
+	override public function arpHeatUp():Bool {
+		super.arpHeatUp();
+
+		if (this.faces.length > 0) return true;
+		var faceInfo:TextureFaceInfo = this.texture.texture.getFaceInfo();
+		var sourceWidth:Int = this.texture.texture.width;
+		var sourceHeight:Int = this.texture.texture.height;
+
+		var faceWidth:Int = this.texture.width;
+		if (faceWidth == 0) faceWidth = sourceWidth;
+		var faceHeight:Int = this.texture.height;
+		if (faceHeight == 0) faceHeight = sourceHeight;
+
+		var isVertical:Bool = this.texture.faceList.isVertical;
+		var x:Int = 0;
+		var y:Int = 0;
+		for (face in this.texture.faceList) {
+			this.nextFaceName(face);
+			for (dir in 0...this.texture.dirs) {
+				this.pushFaceInfo(faceInfo.trim(x, y, faceWidth, faceHeight));
+				if (isVertical) {
+					y += faceHeight;
+					if (y >= sourceHeight) {
+						y = 0;
+						x += faceWidth;
+					}
+				} else {
+					x += faceWidth;
+					if (x >= sourceWidth) {
+						x = 0;
+						y += faceHeight;
+					}
+				}
+			}
+		}
+		return true;
+	}
+}
