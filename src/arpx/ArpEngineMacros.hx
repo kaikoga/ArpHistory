16c2e5870e2b88c1ad1905bd84b1c9b0d27814ce
kaikoga <info@mail.kaikoga.net>
2018-09-07 16:59:57 +0900

    refactor: introducing TargetDef

--- src/arpx/ArpEngineMacros.hx
+++ src/arpx/ArpEngineMacros.hx
@@ -25,60 +25,86 @@ class ArpEngineMacros {
 		components.push(new ArpEngineBackend("arp_storage_backend", ["flash", "openfl", "sys", "stub"]));
 
 		var root:ArpEngineBackend = new ArpEngineBackend("arp_backend", ["heaps", "flash", "openfl", "sys", "stub"]);
-		var rootBackend:String = root.guessBackend(null);
+		var rootTarget:TargetDef = root.guessTarget(null);
 
-		for (component in components) component.guessBackend(rootBackend);
+		for (component in components) component.guessTarget(rootTarget);
 		return;
+	}
+}
+
+class TargetDef {
+
+	private var category:String;
+	public var name(default, null):String;
+
+	public var prettyName(get, never):String;
+	private function get_prettyName():String return '${category} ${name}';
+
+	public var defineName(get, never):String;
+	private function get_defineName():String return '${category}_${name}';
+
+	public function new(category:String, name:String) {
+		this.category = category;
+		this.name = name;
 	}
 }
 
 class ArpEngineBackend {
 
 	private var category:String;
-	private var target:String;
-	private var targets:Array<String>;
+	private var target:TargetDef;
+	private var targets:Array<TargetDef>;
 
-	private function fullName(target:String):String return '${this.category}_${target}';
-
-	public function new(category:String, targets:Array<String>) {
+	public function new(category:String, targetNames:Array<String>) {
 		this.category = category;
-		this.targets = targets;
+		this.targets = targetNames.map(name -> new TargetDef(category, name));
 		this.consumeDefines();
 	}
 
 	private function consumeDefines() {
-		var flagsDefined:Array<String> = [];
+		var definedTargets:Array<TargetDef> = [];
 		for (target in this.targets) {
-			var flagName:String = fullName(target);
-			if (Context.defined(flagName)) flagsDefined.push(target);
+			if (Context.defined(target.defineName)) definedTargets.push(target);
 		}
-		switch (flagsDefined.length) {
+		switch (definedTargets.length) {
 			case 0:
-			case 1: this.target = flagsDefined[0];
-			case _: throw 'Compiler flag conflict: Backends cannot coexist: ${flagsDefined.join(",")}';
+			case 1: this.target = definedTargets[0];
+			case _: throw 'Compiler flag conflict: Backends cannot coexist: ${definedTargets.join(",")}';
 		}
 	}
 
-	public function guessBackend(rootBackend:String):String {
+	public function guessTarget(rootTarget:TargetDef):TargetDef {
 		if (this.target != null) {
-			Sys.println('Using ${this.category} ${this.target}');
+			Sys.println('Using ${this.target.prettyName}');
 			return this.target;
 		}
 
-		if (targets.indexOf(rootBackend) > -1) {
-			// use root backend
-			this.target = rootBackend;
-		} else {
-			// use first available backend
+		if (this.target == null && rootTarget != null) {
+			// use root target
 			for (target in targets) {
-				this.target = target;
-				if (this.target != null && Context.defined(target)) break;
+				if (target.name == rootTarget.name) {
+					this.target = target;
+					break;
+				}
 			}
+		}
+		if (this.target == null) {
+			// use first available target, guess by native defines
+			for (target in targets) {
+				if (Context.defined(target.name)) {
+					this.target = target;
+					break;
+				}
+			}
+		}
+		if (this.target == null) {
+			// use stub
+			this.target = targets[targets.length - 1];
 		}
 
 		if (this.target != null) {
-			Compiler.define(fullName(this.target));
-			Sys.println('Using ${this.category} ${this.target} (auto)');
+			Compiler.define(target.defineName);
+			Sys.println('Using ${this.target.prettyName} (auto)');
 		}
 		return this.target;
 	}
