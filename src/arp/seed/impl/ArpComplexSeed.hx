947f8bf88d5db9d5357b8aaab1362c391d72f88d
kaikoga <info@mail.kaikoga.net>
2018-09-22 21:15:32 +0900

    refactor

--- src/arp/seed/impl/ArpComplexSeed.hx
+++ src/arp/seed/impl/ArpComplexSeed.hx
@@ -7,6 +7,24 @@ class ArpComplexSeed extends ArpSeed {
 
 	private var element:ArpSeedElement;
 
+	private var childrenWithEnv(get, null):Array<ArpSeed>;
+	private function get_childrenWithEnv():Array<ArpSeed> {
+		var value:Array<ArpSeed> = childrenWithEnv;
+		if (value != null) return value;
+		value = [];
+		for (x in this.env.getDefaultSeeds(this.typeName)) {
+			value.push(x);
+		}
+		for (x in this.env.getDefaultClassSeeds(this.typeName, this.className)) {
+			value.push(x);
+		}
+		for (x in this.element.children) {
+			value.push(x);
+		}
+		childrenWithEnv = value;
+		return value;
+	}
+
 	public function new(typeName:String, className:String, name:String, key:String, heat:String, children:Array<ArpSeed>, env:ArpSeedEnv) {
 		super(typeName);
 		this.key = key;
@@ -22,11 +40,5 @@ class ArpComplexSeed extends ArpSeed {
 	override private function get_name():String return element.name;
 	override private function get_heat():String return element.heat;
 
-	override public function iterator():Iterator<ArpSeed> {
-		return new SimpleArrayIterator(
-			this.env.getDefaultSeeds(this.typeName)
-				.concat(this.env.getDefaultClassSeeds(this.typeName, this.className))
-				.concat(this.element.children)
-		);
-	}
+	override public function iterator():Iterator<ArpSeed> return new SimpleArrayIterator(this.childrenWithEnv);
 }
