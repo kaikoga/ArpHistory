a1f7efa9d0665c557d6b888b09e1ce8ff1c20a79
kaikoga <info@mail.kaikoga.net>
2018-11-20 09:22:25 +0900

    refactor: ArpSeedElement is internal

--- src/arp/seed/impl/ArpComplexSeed.hx
+++ src/arp/seed/impl/ArpComplexSeed.hx
@@ -5,7 +5,7 @@ import arp.seed.ArpSeedValueKind;
 
 class ArpComplexSeed extends ArpSeed {
 
-	private var element:ArpSeedElement;
+	private var element:ArpComplexSeedElement;
 
 	private var childrenWithEnv(get, null):Array<ArpSeed>;
 	private function get_childrenWithEnv():Array<ArpSeed> {
@@ -29,7 +29,7 @@ class ArpComplexSeed extends ArpSeed {
 		super(typeName);
 		this.key = key;
 		this.env = env;
-		this.element = new ArpSeedElement(className, name, heat, children);
+		this.element = new ArpComplexSeedElement(className, name, heat, children);
 	}
 
 	override private function get_isSimple():Bool return this.element.isSimple;
@@ -41,4 +41,34 @@ class ArpComplexSeed extends ArpSeed {
 	override private function get_heat():String return element.heat;
 
 	override public function iterator():Iterator<ArpSeed> return new SimpleArrayIterator(this.childrenWithEnv);
+}
+
+class ArpComplexSeedElement {
+
+	public var className(default, null):String;
+	public var name(default, null):String;
+	public var heat(default, null):String;
+
+	public var isSimple(default, null):Bool = true;
+	public var value(default, null):Null<String>;
+	public var valueKind(get, never):ArpSeedValueKind;
+	private function get_valueKind():ArpSeedValueKind return if (this.isSimple) ArpSeedValueKind.Literal else ArpSeedValueKind.None;
+
+	public var children(default, null):Array<ArpSeed>;
+
+	public function new(className:String, name:String, heat:String, children:Array<ArpSeed>) {
+		this.className = className;
+		this.name = name;
+		this.heat = heat;
+		this.children = children;
+		for (child in children) {
+			if (child.typeName == "value") {
+				this.value = child.value;
+			} else {
+				isSimple = false;
+				this.value = null;
+				break;
+			}
+		}
+	}
 }
