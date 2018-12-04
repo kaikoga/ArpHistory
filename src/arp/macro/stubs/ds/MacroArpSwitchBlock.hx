c76bb8dca3675271b46c8a18343d9703c0872ffa
kaikoga <info@mail.kaikoga.net>
2018-12-05 06:53:34 +0900

    handle priority

--- src/arp/macro/stubs/ds/MacroArpSwitchBlock.hx
+++ src/arp/macro/stubs/ds/MacroArpSwitchBlock.hx
@@ -1,11 +1,12 @@
 package arp.macro.stubs.ds;
 
+import haxe.macro.ExprTools;
 import haxe.macro.Expr;
 
 class MacroArpSwitchBlock {
 
 	private var e:Expr;
-	private var cases:Array<Case>;
+	private var cases:Map<String, {c:Case, priority:Int}>;
 	public var eDefault:Expr;
 
 	public function new(e:Expr) {
@@ -13,18 +14,23 @@ class MacroArpSwitchBlock {
 		this.cases = [];
 	}
 
-	public function pushCase(value:ExprOf<String>, pos:Position, caseBlock:Array<Expr>):Void {
-		this.cases.push({
-			values: [value],
-			expr: { pos: pos, expr: ExprDef.EBlock(caseBlock)}
+	public function pushCase(eValue:ExprOf<String>, pos:Position, caseBlock:Array<Expr>, priority:Int = 0):Void {
+		var value:String = ExprTools.getValue(eValue);
+		if (this.cases.exists(value) && this.cases.get(value).priority >= priority) return;
+		this.cases.set(value, {
+			c: {
+				values: [eValue],
+				expr: { pos: pos, expr: ExprDef.EBlock(caseBlock)}
+			},
+			priority: priority
 		});
 	}
 
 	public function toExpr(pos:Position):Expr {
-		return { pos: pos, expr: ExprDef.ESwitch(this.e, this.cases, this.eDefault) };
+		return { pos: pos, expr: ExprDef.ESwitch(this.e, this.toCases(), this.eDefault) };
 	}
 
 	private function toCases():Array<Case> {
-		return this.cases;
+		return [for (v in this.cases) v.c];
 	}
 }
