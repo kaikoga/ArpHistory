64c226e5c62ad4cad0bef6d32773df0d8867e390
kaikoga <info@mail.kaikoga.net>
2018-12-14 02:50:09 +0900

    throw away XML DOM handling

--- src/arp/writers/help/ArpClassHelpPrinter.hx
+++ src/arp/writers/help/ArpClassHelpPrinter.hx
@@ -15,40 +15,46 @@ class ArpClassHelpPrinter {
 
 	public function print():String {
 		var result:String = "";
-		result += '<h1>ArpDomain Reference</h1>';
-		result += '<hr />';
+		result += '<h1>ArpDomain Reference</h1>\n';
+		result += '<hr />\n';
 		result += '<h2>${classInfo.arpType}:${classInfo.className}</h2>\n';
 		result += '<p>${classInfo.fqn}</p>\n';
 		result += '<p>${classInfo.doc}</p>\n';
-		result += '<div><pre>${this.printXml()}</pre></div>\n\n';
+		result += '<div><pre>\n${this.printXml()}\n</pre></div>\n\n';
 		result += '<h2>Fields</h2>\n';
 		result += '${this.printDocs()}\n\n';
 		return result;
 	}
 
 	private function printXml():String {
-		var xml:Xml = Xml.parse('<${classInfo.arpType} name="name" class="${classInfo.className}" />').firstElement();
-		xml.addChild(Xml.parse('<tag value="tagName=tagValue" />').firstElement());
-		this.addFields(xml);
-		return StringTools.htmlEscape(haxe.xml.Printer.print(xml, true));
-	}
-
-	private function addFields(xml:Xml):Void {
+		var result:String = "";
+		result += '<${classInfo.arpType} name="name" class="${classInfo.className}"';
 		for (field in classInfo.fields) {
 			var placeholder:String = getPlaceholder(field);
 			if (field.groupName != null) {
-				populateAttribute(xml, field.groupName, field, placeholder);
-				populateElement(xml, true, field.groupName, field, placeholder);
+				result += populateAttribute(field.groupName, field, placeholder);
 			}
 			if (field.elementName != null) {
-				populateAttribute(xml, field.elementName, field, placeholder);
-				populateElement(xml, false, field.elementName, field, placeholder);
+				result += populateAttribute(field.elementName, field, placeholder);
+			}
+		}
+		result += '/>\n';
+		for (field in classInfo.fields) {
+			var placeholder:String = getPlaceholder(field);
+			if (field.groupName != null) {
+				result += populateElement(true, field.groupName, field, placeholder);
+			}
+			if (field.elementName != null) {
+				result += populateElement(false, field.elementName, field, placeholder);
 			}
 		}
 		for (field in classInfo.fields) {
 			var placeholder:String = getPlaceholder(field);
-			populateTextNode(xml, field, placeholder);
+			result += populateTextNode(field, placeholder);
 		}
+		result += '</${classInfo.arpType}>\n';
+
+		return StringTools.htmlEscape(result);
 	}
 
 	private function getPlaceholder(field:ArpFieldInfo):String {
@@ -61,64 +67,67 @@ class ArpClassHelpPrinter {
 		return placeholder;
 	}
 
-	private function populateAttribute(xml:Xml, name:String, field:ArpFieldInfo, placeholder:String) {
-		if (!field.isCollection) xml.set('${name}', placeholder);
+	private function populateAttribute(name:String, field:ArpFieldInfo, placeholder:String):String {
+		if (field.isCollection) return "";
+		return '$name = "$placeholder"';
 	}
 
-	private function populateElement(xml:Xml, mayGroup:Bool, name:String, field:ArpFieldInfo, placeholder:String) {
-		if (mayGroup && field.isCollection) {
-			var group:Xml = Xml.createElement(field.groupName);
-			xml.addChild(group);
-			xml = group;
-			name = field.arpType;
-		}
+	private function populateElement(mayGroup:Bool, name:String, field:ArpFieldInfo, placeholder:String):String {
+		var willGroup = mayGroup && field.isCollection;
+		if (willGroup) name = field.arpType;
 
-		var node:Xml;
-		switch (field.fieldKind) {
-			case ArpFieldKind.PrimBool, ArpFieldKind.PrimInt, ArpFieldKind.PrimFloat, ArpFieldKind.PrimString:
-				node = Xml.parse('<${name} value="${placeholder}" />').firstElement();
-			case ArpFieldKind.StructKind:
-				switch (field.arpType.toString()) {
-					case "Range":
-						node = Xml.parse('<${name} min="minValue" max="maxValue" />').firstElement();
-					case "Color":
-						node = Xml.parse('<${name} value="${placeholder}" />').firstElement();
-					case "Position":
-						node = Xml.parse('<${name} x="x" y="y" z="z" dir="dir" />').firstElement();
-					case "HitArea":
-						node = Xml.parse('<${name} left="0" right="1" top="0" bottom="1" hind="0" fore="1" />').firstElement();
-					case "Params":
-						node = Xml.parse('<${name} value="${placeholder}" />').firstElement();
-					case some:
-						node = Xml.parse('<${name} ${some}="${placeholder}">${placeholder}</${name}>').firstElement();
-				}
-			case ArpFieldKind.ReferenceKind:
-				node = Xml.parse('<${name} ref="${placeholder}" />').firstElement();
-		}
-
+		var key:String = "";
 		switch (field.fieldDs) {
 			case ArpFieldDs.Scalar:
 			case ArpFieldDs.StdArray, ArpFieldDs.StdList, ArpFieldDs.DsISet, ArpFieldDs.DsIList:
 			case ArpFieldDs.StdMap, ArpFieldDs.DsIMap, ArpFieldDs.DsIOmap:
-				node.set("key", "key");
+				key = 'key="key" ';
 			default:
 		}
 
-		xml.addChild(node);
+		var value:String = switch (field.fieldKind) {
+			case ArpFieldKind.PrimBool, ArpFieldKind.PrimInt, ArpFieldKind.PrimFloat, ArpFieldKind.PrimString:
+				'value="${placeholder}" ';
+			case ArpFieldKind.StructKind:
+				switch (field.arpType.toString()) {
+					case "Range":
+						'min="minValue" max="maxValue" ';
+					case "Color":
+						'value="#000000@00 $placeholder" ';
+					case "Position":
+						'x="x" y="y" z="z" dir="dir" ';
+					case "HitArea":
+						'left="0" right="1" top="0" bottom="1" hind="0" fore="1" ';
+					case "Params":
+						'value="params $placeholder" ';
+					case some:
+						'value="$placeholder" ';
+				}
+			case ArpFieldKind.ReferenceKind:
+				'ref="$placeholder" ';
+		}
+
+		var result:String = '<$name $key$value/>';
+
+		if (willGroup) return '<${field.groupName}>\n\t$result\n</${field.groupName}>\n';
+		return result + "\n";
 	}
 
-	private function populateTextNode(parent:Xml, field:ArpFieldInfo, placeholder:String) {
-		if (field.groupName == "value") parent.addChild(Xml.createPCData(placeholder));
+	private function populateTextNode(field:ArpFieldInfo, placeholder:String):String {
+		if (field.groupName != "value") return "";
+		return '\tplaceholder\n';
 	}
 
 	private function printDocs():String {
-		var xml:Xml = Xml.createElement('section');
+		var result:String = "";
+		result += '<section>\n';
 		for (field in classInfo.fields) {
-			xml.addChild(Xml.parse('<h3>${field.nativeName}</h3>').firstElement());
-			xml.addChild(Xml.parse('<section>${field.doc}</section>').firstElement());
-			xml.addChild(Xml.createElement('hr'));
+			result += '<h3>${field.nativeName}</h3>\n';
+			result += '<section>${field.doc}</section>\n';
+			result += '<hr />\n';
 		}
-		return haxe.xml.Printer.print(xml, true);
+		result += '</section>\n';
+		return result;
 	}
 
 }
