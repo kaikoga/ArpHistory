d2eb2b9ff27a0ce15b5c236173e1aa917a827702
kaikoga <info@mail.kaikoga.net>
2018-12-19 00:24:16 +0900

    omit placeholder arguments

--- src/arp/writers/help/ArpClassHelpPrinter.hx
+++ src/arp/writers/help/ArpClassHelpPrinter.hx
@@ -36,48 +36,35 @@ class ArpClassHelpPrinter {
 		var result:StringBuffer = 0;
 		result += '<${classInfo.arpType} name="name" class="${classInfo.className}" ';
 		for (field in classInfo.fields) {
-			var placeholder:String = getPlaceholder(field);
 			if (field.groupName != null) {
-				result += populateAttribute(field.groupName, field, placeholder);
+				result += populateAttribute(field.groupName, field);
 			}
 			if (field.elementName != null) {
-				result += populateAttribute(field.elementName, field, placeholder);
+				result += populateAttribute(field.elementName, field);
 			}
 		}
 		result += '/>\n';
 		for (field in classInfo.fields) {
-			var placeholder:String = getPlaceholder(field);
 			if (field.groupName != null) {
-				result += populateElement(true, field.groupName, field, placeholder);
+				result += populateElement(true, field.groupName, field);
 			}
 			if (field.elementName != null) {
-				result += populateElement(false, field.elementName, field, placeholder);
+				result += populateElement(false, field.elementName, field);
 			}
 		}
 		for (field in classInfo.fields) {
-			var placeholder:String = getPlaceholder(field);
-			result += populateTextNode(field, placeholder);
+			result += populateTextNode(field);
 		}
-		result <<= '</${classInfo.arpType}>';
+		result += '</${classInfo.arpType}>';
 
 		return StringTools.htmlEscape(result);
 	}
 
-	private function getPlaceholder(field:ArpFieldInfo):String {
-		var placeholder:String = field.arpType.toString() + " " + field.nativeName;
-		/*
-		if (field.defaultValue) {
-			placeholder += "=" + field.defaultValue;
-		}
-		*/
-		return placeholder;
-	}
-
-	private function populateAttribute(name:String, field:ArpFieldInfo, placeholder:String):String {
+	private function populateAttribute(name:String, field:ArpFieldInfo):String {
 		if (field.isCollection) return "";
 		switch (field.fieldKind) {
 			case ArpFieldKind.PrimBool, ArpFieldKind.PrimInt, ArpFieldKind.PrimFloat, ArpFieldKind.PrimString:
-				return '$name="$placeholder" ';
+				return '$name="${field.arpType}" ';
 			case ArpFieldKind.StructKind:
 				var structInfo:ArpClassInfo = this.domainInfo.findStructInfo(field.arpType);
 				if (structInfo != null) {
@@ -85,13 +72,13 @@ class ArpClassHelpPrinter {
 						return '$name="${structInfo.stringPlaceholder}" ';
 					}
 				}
-				return '$name="$placeholder" ';
+				return '$name="${field.arpType}" ';
 			case ArpFieldKind.ReferenceKind:
-				return '$name="$placeholder" ';
+				return '$name="${field.arpType}" ';
 		}
 	}
 
-	private function populateElement(mayGroup:Bool, name:String, field:ArpFieldInfo, placeholder:String):String {
+	private function populateElement(mayGroup:Bool, name:String, field:ArpFieldInfo):String {
 		var willGroup = mayGroup && field.isCollection;
 		if (willGroup) name = field.arpType;
 
@@ -107,9 +94,9 @@ class ArpClassHelpPrinter {
 		var value:String;
 		switch (field.fieldKind) {
 			case ArpFieldKind.PrimBool, ArpFieldKind.PrimInt, ArpFieldKind.PrimFloat, ArpFieldKind.PrimString:
-				value = 'value="$placeholder" ';
+				value = 'value="${field.arpType}" ';
 			case ArpFieldKind.StructKind:
-				value = 'value="$placeholder" ';
+				value = 'value="${field.arpType}" ';
 				var structInfo:ArpClassInfo = this.domainInfo.findStructInfo(field.arpType);
 				if (structInfo != null) {
 					if (structInfo.seedPlaceholder != null) {
@@ -120,7 +107,7 @@ class ArpClassHelpPrinter {
 					}
 				}
 			case ArpFieldKind.ReferenceKind:
-				value = 'ref="$placeholder" ';
+				value = 'ref="${field.arpType}" ';
 		}
 
 		var result:String = '<$name $key$value/>';
@@ -129,9 +116,9 @@ class ArpClassHelpPrinter {
 		return result + "\n";
 	}
 
-	private function populateTextNode(field:ArpFieldInfo, placeholder:String):String {
+	private function populateTextNode(field:ArpFieldInfo):String {
 		if (field.groupName != "value") return "";
-		return '\tplaceholder\n';
+		return '\t${field.groupName}\n';
 	}
 
 	private function printDocs():String {
